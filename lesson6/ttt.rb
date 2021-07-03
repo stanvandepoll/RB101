5015e6edfae8e88d69d9266667866d5afc4dcacc
Stan van de Poll <swvandepoll@gmail.com>
2021-07-03 12:23:59 +0200

    Only allow integers for win limit

--- lesson6/ttt.rb
+++ lesson6/ttt.rb
@@ -180,7 +180,7 @@ def input_win_limit
   win_limit = ''
   loop do
     win_limit = gets.chomp.to_i
-    break if (1..20).cover?(win_limit)
+    break if (1..20).to_a.include?(win_limit)
 
     prompt 'Invalid choice, please choose again'
   end
