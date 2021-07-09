78b351c94c7f6ab736c34e2d1362cd709280cbfa
Stan van de Poll <swvandepoll@gmail.com>
2021-07-09 10:48:17 +0200

    Add alternative words to digits method

--- small_problems/medium_1/7.rb
+++ small_problems/medium_1/7.rb
@@ -3,9 +3,22 @@ DIGIT_HASH = {
   'five' => '5', 'six' => '6', 'seven' => '7', 'eight' => '8', 'nine' => '9'
 }.freeze
 
-def word_to_digit(words)
+def words_to_digits(words)
   DIGIT_HASH.each do |word, number|
     words.gsub!(/\b#{word}\b/, number)
   end
   words
+end
+
+def alt_words_to_digits(words)
+  words.split.map do |word|
+    word_to_digit(word)
+  end.join(' ')
+end
+
+def word_to_digit(word)
+  word_only_letters = word.gsub(/[^a-zA-Z]/, '')
+  return word unless DIGIT_HASH.keys.include?(word_only_letters)
+
+  word.gsub(word_only_letters, DIGIT_HASH[word_only_letters])
 end
